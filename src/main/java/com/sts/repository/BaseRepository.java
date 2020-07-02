691be7cde59a484659252b06b8ebb2e83b98fd87
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-07-02 14:50:34 +0530

    1 Creating Spring Data Rest Project

---
+++ src/main/java/com/sts/repository/BaseRepository.java
@@ -0,0 +1,7 @@
+package com.sts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+@NoRepositoryBean
+public interface BaseRepository<T,I> extends JpaRepository<T, I> {
+}
