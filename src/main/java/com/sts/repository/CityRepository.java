dbac0d6366c14db6841edaa76009cbc5b19d2c4e
Bhagyashree Chavan <bhagyashree.chavan@sts.in>
2020-07-02 15:01:06 +0530

    5 Derived JPA Query
    Using search API for derived JPA query
    Using @RestResource to modify default path

--- src/main/java/com/sts/repository/CityRepository.java
+++ src/main/java/com/sts/repository/CityRepository.java
@@ -1,6 +1,12 @@
 package com.sts.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.sts.entity.City;
 public interface CityRepository extends BaseRepository<City, Long>{
-	
+	@RestResource(path="names")
+	Optional<City> findByName(@Param("name")String name);
 }
--- src/main/java/com/sts/repository/CountryRepository.java
+++ src/main/java/com/sts/repository/CountryRepository.java
@@ -1,7 +1,11 @@
 package com.sts.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.repository.query.Param;
+
 import com.sts.entity.Country;
 
 public interface CountryRepository extends BaseRepository<Country, Long>{
-	
+	Optional<Country> findByName(@Param("name") String name);
 }
